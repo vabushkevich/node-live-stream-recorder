f22d3b1632b3a3ed0ad76c7527dbfda6fe7e16eb
Vyacheslav Abushkevich <abshkvch@gmail.com>
2021-09-17 16:19:57 +0300

    Define postStart() in Twitch

--- src/stream-page/Twitch.js
+++ src/stream-page/Twitch.js
@@ -10,6 +10,24 @@ class Twitch extends StreamPage {
     closeDialogBox();
   }
 
+  async postStart() {
+    const selectHLS = async () => {
+      const menu = await this.getQualityMenu();
+      const menuItems = await menu.$$(":scope > *");
+      const hslMenuItem = await (async () => {
+        for (const menuItem of menuItems) {
+          const menuItemText = await menuItem.evaluate((elem) => elem.textContent);
+          if (menuItemText == "HLS") return menuItem;
+        }
+      })();
+      if (!hslMenuItem) return Promise.reject();
+      await this.page.waitForTimeout(500)
+        .then(() => hslMenuItem.evaluate((elem) => elem.click()));
+    };
+
+    await selectHLS().catch(() => Promise.reject("Can't select HLS"));
+  }
+
   getQualityMenu() {
     return this.page
       .waitForSelector(".icon-gears ~ .menu .menu-content");
