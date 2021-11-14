6d1b4228896c7bea9c3453346c10e5d27a898be0
Vyacheslav Abushkevich <abshkvch@gmail.com>
2021-11-14 23:14:05 +0300

    Take screenshot from local file

--- src/StreamRecording.js
+++ src/StreamRecording.js
@@ -69,12 +69,11 @@ class StreamRecording extends EventEmitter {
           resolveIn(NO_DATA_TIMEOUT)
             .then(() => Promise.reject(new Error("Timeout while getting a stream")))
         ]);
-        this.m3u8Url = stream.url;
 
         const fileStem = this.buildName(new Date());
-        const outFilePath = path.join(RECORDINGS_ROOT, this.name, `${fileStem}.mkv`);
+        this.outFilePath = path.join(RECORDINGS_ROOT, this.name, `${fileStem}.mkv`);
 
-        this.m3u8Fetcher = new M3u8Fetcher(stream.url, outFilePath);
+        this.m3u8Fetcher = new M3u8Fetcher(stream.url, this.outFilePath);
         this.setUpM3u8FetcherEventHandlers();
         this.setUpStreamLifeCheck();
         this.m3u8Fetcher.start();
@@ -132,8 +131,15 @@ class StreamRecording extends EventEmitter {
       if (this.getTimeLeft() <= 0) this.stop();
     });
 
-    this.m3u8Fetcher.on("request", throttle(() => {
-      saveFrame(this.m3u8Url, this.screenshotPath, { quality: 31 })
+    this.m3u8Fetcher.on("duration", throttle(() => {
+      saveFrame(
+        this.outFilePath,
+        this.screenshotPath,
+        {
+          position: `${this.recordedDuration - 2000}ms`,
+          quality: 31,
+        }
+      )
         .catch(err => this.log(`Can't take screenshot: ${err}`));
     }, SCREENSHOT_FREQ));
 
