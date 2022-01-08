ef8c801d4331232c62b2474db943cd98884834f7
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-01-08 15:49:04 +0300

    Add isValidUrl()

--- src/utils.js
+++ src/utils.js
@@ -83,6 +83,15 @@ async function retry(func, retryDelays, callback) {
   }
 }
 
+function isValidUrl(url) {
+  try {
+    new URL(url);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
 module.exports = {
   saveFrame,
   resolveIn,
