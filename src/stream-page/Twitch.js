1736caa2cf1d4ce9feac6f8960039f8d342e1b4a
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-01-12 11:39:49 +0300

    Update Twitch handle

--- src/stream-page/Twitch.js
+++ src/stream-page/Twitch.js
@@ -3,23 +3,8 @@ const StreamPage = require('lib/stream-page/StreamPage');
 class Twitch extends StreamPage {
   async getM3u8Url() {
     const page = await this.open();
-
-    page.waitForFunction(
-      async () => {
-        const elem = [...document.querySelectorAll("li > span")]
-          .find((elem) => elem.textContent == "HLS");
-        if (elem) {
-          await new Promise((resolve) => setTimeout(resolve, 500));
-          elem.click();
-          return true;
-        }
-      },
-      { polling: "mutation" }
-    )
-      .catch(() => { });
-
     const res = await page.waitForResponse((res) =>
-      new URL(res.url()).pathname.endsWith("playlist.m3u8")
+      new URL(res.url()).pathname.endsWith(".m3u8")
     );
     this.close();
     return res.url();
