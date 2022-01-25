0af7448ba26d67db0701f31abd1bee1db4fa71ea
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-01-25 12:46:41 +0300

    Declare constants with const

--- src/config.js
+++ src/config.js
@@ -1,24 +1,31 @@
 require('dotenv').config();
 
-const config = {
-  NO_DATA_TIMEOUT: 30 * 1000,
-  SCREENSHOT_FREQ: 20 * 1000,
-  MAX_OPEN_PAGES: 5,
-  FETCH_HEADERS: {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
-  },
-  SCREENSHOTS_ROOT: "./site/screenshots/",
-  LOG_PATH: "./server.log",
-  RECORDINGS_ROOT: process.env.RECORDINGS_ROOT,
-  BROWSER_PATH: process.env.BROWSER_PATH,
-}
+const NO_DATA_TIMEOUT = 30 * 1000;
+const SCREENSHOT_FREQ = 20 * 1000;
+const MAX_OPEN_PAGES = 5;
+const FETCH_HEADERS = {
+  "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
+};
+const SCREENSHOTS_ROOT = "./site/screenshots/";
+const LOG_PATH = "./server.log";
+const RECORDINGS_ROOT = process.env.RECORDINGS_ROOT;
+const BROWSER_PATH = process.env.BROWSER_PATH;
 
-if (!config.RECORDINGS_ROOT) {
+if (!RECORDINGS_ROOT) {
   throw new Error("Env var RECORDINGS_ROOT must be set");
 }
 
-if (!config.BROWSER_PATH) {
+if (!BROWSER_PATH) {
   throw new Error("Env var BROWSER_PATH must be set");
 }
 
-module.exports = config;
+module.exports = {
+  NO_DATA_TIMEOUT,
+  SCREENSHOT_FREQ,
+  MAX_OPEN_PAGES,
+  FETCH_HEADERS,
+  SCREENSHOTS_ROOT,
+  LOG_PATH,
+  RECORDINGS_ROOT,
+  BROWSER_PATH,
+};
