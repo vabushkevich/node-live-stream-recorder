b2e1edbf6225d679b867315d0a938f15a27ef7c7
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-02-25 19:17:17 +0300

    Rename constants

--- client/constants.js
+++ client/constants.js
@@ -1,3 +1,3 @@
-const HOSTNAME = process.env.SERVER_HOSTNAME;
-const PORT = process.env.SERVER_PORT;
-export const API_BASE_URL = `http://${HOSTNAME}:${PORT}/api/v1`;
+const API_HOSTNAME = process.env.SERVER_HOSTNAME;
+const API_PORT = process.env.SERVER_PORT;
+export const API_BASE_URL = `http://${API_HOSTNAME}:${API_PORT}/api/v1`;
