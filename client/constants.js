41328edf062af2dc11ec4fe1f1692d1ce04f7985
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-03-25 11:25:35 +0300

    Change API_BASE_URL value

--- client/constants.js
+++ client/constants.js
@@ -1,3 +1,3 @@
 const API_HOSTNAME = process.env.SERVER_HOSTNAME;
 const API_PORT = process.env.SERVER_PORT;
-export const API_BASE_URL = `http://${API_HOSTNAME}:${API_PORT}/api/v1`;
+export const API_BASE_URL = `/api/v1`;
