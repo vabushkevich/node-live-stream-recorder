87a0b4a8b8430817012f430c1d49b9d6f98b85e3
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-03-31 12:24:22 +0300

    Inport SERVER_PORT to webpack.config.js from .env

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+require("dotenv").config({ path: "../.env" });
 
 const BUILD_OUT_ROOT = path.resolve(__dirname, "build");
+const SERVER_PORT = process.env.SERVER_PORT || 5370;
 
 module.exports = {
   entry: "./src/index.jsx",
@@ -13,6 +15,9 @@ module.exports = {
       directory: path.join(BUILD_OUT_ROOT, "screenshots"),
       publicPath: "/screenshots",
       watch: false,
+    },
+    proxy: {
+      "/api": `http://localhost:${SERVER_PORT}`,
     },
   },
   module: {
