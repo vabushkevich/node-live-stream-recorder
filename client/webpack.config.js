63788044456cbc5c9b3b9449c14ecda524123109
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-05-03 14:29:27 +0300

    Add sass support

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,8 +23,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.jsx$/i,
