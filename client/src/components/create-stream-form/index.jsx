0f92abdd58cfd1b43aff109d105309275481fb2b
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-05-17 12:29:21 +0300

    Add styles to CreateStreamForm component

--- client/src/components/create-stream-form/index.jsx
+++ client/src/components/create-stream-form/index.jsx
@@ -3,6 +3,7 @@ import { Button } from "../button";
 import { Input } from "../input";
 import { Select } from "../select";
 import { Card } from "../card";
+import "./index.scss";
 
 export class CreateStreamForm extends React.Component {
   constructor(props) {
@@ -39,53 +40,53 @@ export class CreateStreamForm extends React.Component {
     } = this.state;
 
     return (
-      <div className="row mb-3">
-        <div className="col-lg-7">
-          <Card title="Record new stream">
-            <form onSubmit={this.handleSubmit}>
-              <div className="form-row">
-                <div className="form-group col">
-                  <Input
-                    id="inputURL"
-                    label="URL"
-                    name="url"
-                    type="url"
-                    value={url}
-                    onChange={this.handleInputChange}
-                  />
-                </div>
-                <div className="form-group col-3 col-md-2">
-                  <Input
-                    id="inputDuration"
-                    label="Minutes"
-                    min="1"
-                    name="duration"
-                    type="number"
-                    value={duration}
-                    onChange={this.handleInputChange}
-                  />
-                </div>
-                <div className="form-group col-4 col-md-3">
-                  <Select
-                    id="selectResolution"
-                    name="resolution"
-                    value={resolution}
-                    label="Resolution"
-                    onChange={this.handleInputChange}
-                  >
-                    <option value="10000">Highest</option>
-                    <option value="1080">1080p</option>
-                    <option value="720">720p</option>
-                    <option value="480">480p</option>
-                    <option value="360">360p</option>
-                    <option value="0">Lowest</option>
-                  </Select>
-                </div>
+      <div className="create-stream-form">
+        <Card title="Record new stream">
+          <form onSubmit={this.handleSubmit}>
+            <div className="create-stream-form__row">
+              <div className="create-stream-form__url">
+                <Input
+                  id="inputURL"
+                  label="URL"
+                  name="url"
+                  type="url"
+                  value={url}
+                  onChange={this.handleInputChange}
+                />
               </div>
-              <Button type="submit">Record</Button>
-            </form>
-          </Card>
-        </div>
+              <div className="create-stream-form__duration">
+                <Input
+                  id="inputDuration"
+                  label="Minutes"
+                  min="1"
+                  name="duration"
+                  type="number"
+                  value={duration}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div className="create-stream-form__resolution">
+                <Select
+                  id="selectResolution"
+                  name="resolution"
+                  value={resolution}
+                  label="Resolution"
+                  onChange={this.handleInputChange}
+                >
+                  <option value="10000">Highest</option>
+                  <option value="1080">1080p</option>
+                  <option value="720">720p</option>
+                  <option value="480">480p</option>
+                  <option value="360">360p</option>
+                  <option value="0">Lowest</option>
+                </Select>
+              </div>
+              <div>
+                <Button type="submit">Record</Button>
+              </div>
+            </div>
+          </form>
+        </Card>
       </div>
     );
   }
