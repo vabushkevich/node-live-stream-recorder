bd658a7fe1ab1358eb85e6b9e803d7ec13d4f765
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-05-23 15:23:02 +0300

    Remove bootstrap

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 
 import { App } from "./App";
 
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./sass/_global.scss";
 import "./sass/_forms.scss";
 import "./sass/_typography.scss";
