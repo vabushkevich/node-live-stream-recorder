f67cfbf9d170cea70669895c9daf4d0d6aede51e
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-05-26 15:45:10 +0300

    Add normalize.css

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 
 import { App } from "./App";
 
+import "normalize.css";
 import "./sass/_global.scss";
 import "./sass/_forms.scss";
 import "./sass/_typography.scss";
