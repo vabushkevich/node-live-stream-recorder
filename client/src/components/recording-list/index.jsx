71146a380716a17101003467757e34cd633e027d
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-06-02 16:23:30 +0300

    Show placeholder when RecordingList is empty

--- client/src/components/recording-list/index.jsx
+++ client/src/components/recording-list/index.jsx
@@ -12,21 +12,27 @@ export function RecordingList({
   return (
     <div className="recording-list">
       <Card title="Recordings">
-        <ol>
-          {recordings.map((recording) => {
-            const { id } = recording;
-            return (
-              <li className="recording-list__item" key={id}>
-                <Recording
-                  {...recording}
-                  onStop={onRecordingStop.bind(null, id)}
-                  onProlong={onRecordingProlong.bind(null, id)}
-                  onClose={onRecordingClose.bind(null, id)}
-                />
-              </li>
-            );
-          })}
-        </ol>
+        {recordings.length > 0 ? (
+          <ol>
+            {recordings.map((recording) => {
+              const { id } = recording;
+              return (
+                <li className="recording-list__item" key={id}>
+                  <Recording
+                    {...recording}
+                    onStop={onRecordingStop.bind(null, id)}
+                    onProlong={onRecordingProlong.bind(null, id)}
+                    onClose={onRecordingClose.bind(null, id)}
+                  />
+                </li>
+              );
+            })}
+          </ol>
+        ) : (
+          <div className="recording-list__placeholder">
+            There are no recordings yet... Start one using the form above.
+          </div>
+        )}
       </Card>
     </div>
   );
