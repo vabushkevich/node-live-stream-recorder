9ee7ff8ce41c477155f53fc409eea03393e0a913
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-06-15 18:41:15 +0300

    Use autoprefixer

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,6 +31,7 @@ module.exports = {
         use: [
           devMode ? "style-loader" : MiniCssExtractPlugin.loader,
           "css-loader",
+          "postcss-loader",
           "sass-loader",
         ],
       },
