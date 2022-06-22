10cc5fd32db7243df9aea010884d1570f18b9e9f
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-06-22 15:42:06 +0300

    Make Badge color optional

--- client/src/components/badge/index.tsx
+++ client/src/components/badge/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import "./index.scss";
 
 type BadgeProps = {
-  color: "primary" | "secondary" | "dark";
+  color?: "primary" | "secondary" | "dark";
   children?: React.ReactNode;
 };
 
 export function Badge({
-  color,
+  color = "primary",
   children,
 }: BadgeProps) {
   return <span className={`badge badge--${color}`}>{children}</span>;
