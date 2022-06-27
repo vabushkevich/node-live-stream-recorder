270f0ed3f735a2e680f639fb014a5411e2b9afaf
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-06-27 15:04:53 +0300

    Turn Card title and body into BEM elements

--- client/src/components/card/index.tsx
+++ client/src/components/card/index.tsx
@@ -12,8 +12,8 @@ export function Card({
 }: CardProps) {
   return (
     <div className="card">
-      {title && <div className="card-title">{title}</div>}
-      <div className="card-body">{children}</div>
+      {title && <div className="card__title">{title}</div>}
+      <div className="card__body">{children}</div>
     </div>
   );
 }
