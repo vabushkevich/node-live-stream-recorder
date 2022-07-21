63d4952def53affb154d44c126451ff0e2c75675
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-07-21 16:25:09 +0300

    Allow creation of StreamPage objects

--- server/src/services/stream-page/createStreamPage.ts
+++ server/src/services/stream-page/createStreamPage.ts
@@ -1,8 +1,9 @@
 import { YouTube, Twitch } from ".";
+import { StreamPage } from "./StreamPage";
 
 export function createStreamPage(url: string) {
   if (url.includes("youtube.com")) return new YouTube(url);
   if (url.includes("twitch.tv")) return new Twitch(url);
 
-  throw new Error(`Can't get handle for url: ${url}`);
+  return new StreamPage(url);
 }
