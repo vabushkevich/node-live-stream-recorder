1e1e93da9ebfa71066a8f6294e13b02d7be8eeac
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-07-21 15:06:21 +0300

    Catch m3u8 in streamPage.getM3u8Url by default

--- server/src/services/stream-page/StreamPage.ts
+++ server/src/services/stream-page/StreamPage.ts
@@ -40,8 +40,13 @@ export class StreamPage {
     return stream;
   }
 
-  getM3u8Url(): string | Promise<string> {
-    throw new Error("Method not implemented.");
+  async getM3u8Url() {
+    const page = await this.open();
+    const res = await page.waitForResponse((res) =>
+      new URL(res.url()).pathname.endsWith(".m3u8")
+    );
+    this.close();
+    return res.url();
   }
 
   async open() {
